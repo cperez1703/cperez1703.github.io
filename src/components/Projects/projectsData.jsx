3dc83987a4b9ff8a488dadf530ffe59406c2205a
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-06-01 00:54:24 -0400

    image fix

--- src/components/Projects/projectsData.jsx
+++ src/components/Projects/projectsData.jsx
@@ -9,7 +9,7 @@ import {
       image: image1,
       transitionImage: image2,
       title:
-        "Project Description.",
+        "Project Description. Test",
       codeLink: "https://github.com/cperez1703/cperez1703.github.io",
       liveLink: "https://cperez1703.github.io/",
       techStack: ["React", "MongoDB", "Node.js"],
