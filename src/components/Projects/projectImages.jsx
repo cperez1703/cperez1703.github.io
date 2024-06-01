f3a3ab838d4bddbd0b91d9c088cfd74f6c6bea4a
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-06-01 00:50:04 -0400

    fixed project image

--- src/components/Projects/projectImages.jsx
+++ src/components/Projects/projectImages.jsx
@@ -1,5 +1,5 @@
 import image1 from "../../assets/projects/examplecpwebsite.png";
-import image2 from "../../assets/projects/examplecpwebsite.png";
+import image2 from "../../assets/projects/examplecpwebsite1.png";
 
 export {
   image1,
